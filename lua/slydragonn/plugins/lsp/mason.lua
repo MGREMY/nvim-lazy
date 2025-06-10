e80966dd19c005d07f35c50ec0e4ab9d94773f05
MGREMY <miguel.gremy72+pro@gmail.com>
2025-06-10 17:55:49 +0200

    update to csharp_ls

--- lua/slydragonn/plugins/lsp/mason.lua
+++ lua/slydragonn/plugins/lsp/mason.lua
@@ -1,33 +1,33 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		require("mason").setup()
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    require("mason").setup()
 
-		require("mason-lspconfig").setup({
-			automatic_installation = true,
-			ensure_installed = {
-				"cssls",
-				"csharp-language-server",
-				"eslint",
-				"html",
-				"jsonls",
-				"ts_ls",
-				"tailwindcss",
-				"gopls",
-				"golangci_lint_ls",
-			},
-		})
+    require("mason-lspconfig").setup({
+      automatic_installation = true,
+      ensure_installed = {
+        "cssls",
+        "csharp_ls",
+        "eslint",
+        "html",
+        "jsonls",
+        "ts_ls",
+        "tailwindcss",
+        "gopls",
+        "golangci_lint_ls",
+      },
+    })
 
-		require("mason-tool-installer").setup({
-			ensure_installed = {
-				"prettier",
-				"stylua", -- lua formatter
-				"eslint_d",
-			},
-		})
-	end,
+    require("mason-tool-installer").setup({
+      ensure_installed = {
+        "prettier",
+        "stylua", -- lua formatter
+        "eslint_d",
+      },
+    })
+  end,
 }
